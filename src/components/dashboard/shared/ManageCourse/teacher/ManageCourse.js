8c73782cc564ac0fcc529af93513c1c2ea884e4d
A.K.M Fozlol Hoq <akmfozlolhoq@gmail.com>
2023-08-04 14:21:37 +0600

    implement manage course for teacher

--- src/components/dashboard/shared/ManageCourse/teacher/ManageCourse.js
+++ src/components/dashboard/shared/ManageCourse/teacher/ManageCourse.js
@@ -1,9 +1,26 @@
-import React from "react";
+import ViewCourses from "../../ViewCourses/ViewCourses";
+import { useRouter } from "next/router";
 
 const ManageCourse = () => {
+  const { push } = useRouter();
+  const redirectCreateCourse = () => {
+    push(`/dashboard/create-course`);
+  };
+
   return (
-    <div>
-      <h1>This is manage course teacher</h1>
+    <div className="flex flex-col items-center justify-start min-h-screen pt-8">
+      <div className="w-2/3 flex justify-between items-center">
+        <h1 className="text-4xl font-bold">All courses</h1>
+        <button
+          className="px-4 py-2 bg-blue-500 text-white font-semibold rounded"
+          onClick={redirectCreateCourse}
+        >
+          Create new course
+        </button>
+      </div>
+      <div className="mt-8">
+        <ViewCourses redirectDetails={true} />
+      </div>
     </div>
   );
 };
